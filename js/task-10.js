88a9e162809ccf9997c43d91acd7dcfe7654c3e9
Eremey2410 <2410esv@gmail.com>
2022-07-23 18:13:04 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,25 @@
-const createEl = document.querySelector;
+const refs = {
+  amount: document.querySelector("#controls input"),
+  createBtn: document.querySelector("button[ data-create]"),
+  destroyBtn: document.querySelector("button[ data-destroy]"),
+  boxes: document.querySelector("#boxes"),
+};
+refs.createBtn.addEventListener("click", createBoxes);
+refs.destroyBtn.addEventListener("click", destroyBoxes);
+refs.amount.addEventListener("input", getAmount);
+
+function getAmount() {
+  console.log(refs.amount.value);
+}
+
+function createBoxes(amount) {
+  refs.amount.value = document.createElement("div");
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function destroyBoxes() {
+  refs.boxes.innerHTML = "";
 }
