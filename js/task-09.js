176fd78dd9b04db797053f81d1744ce571d80078
Eremey2410 <2410esv@gmail.com>
2022-07-24 14:01:46 +0300

    corrected task-09

--- js/task-09.js
+++ js/task-09.js
@@ -2,9 +2,15 @@ const refs = {
   color: document.querySelector(".color"),
   changeColor: document.querySelector(".change-color"),
 };
-refs.changeColor.addEventListener("click", () => {
-  document.body.style.backgroundColor = `#${Math.floor(
-    Math.random() * 16777215
-  ).toString(16)}`;
+refs.changeColor.addEventListener("click", getColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function getColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
   refs.color.textContent = document.body.style.backgroundColor;
-});
+}
